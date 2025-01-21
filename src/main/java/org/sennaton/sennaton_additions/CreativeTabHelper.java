6e74acfeac90b3413107ded43f403c993584d5be
Sennaton <sebastian13244@gmail.com>
2025-01-21 10:32:25 -0800

    Compat changes

--- src/main/java/org/sennaton/sennaton_additions/CreativeTabHelper.java
+++ src/main/java/org/sennaton/sennaton_additions/CreativeTabHelper.java
@@ -2,17 +2,27 @@ package org.sennaton.sennaton_additions;
 
 //import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 //import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.ItemLike;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import org.sennaton.sennaton_additions.SennatonItems.SennatonItems;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 //import static net.minecraft.world.item.CreativeModeTabs.BUILDING_BLOCKS;
-
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class CreativeTabHelper {
+    private static ArrayList<pair> list = new ArrayList<>();
 
      //private static void addi(ForgeItemGroupEntries entries, ItemStack item){
           //entries.accept(item);
@@ -20,6 +30,20 @@ public class CreativeTabHelper {
 
     public static void register(RegistryObject<Item> item, ResourceKey<CreativeModeTab> tab){
         //Forge
-            //content.accept(SennatonItems.crystalEdge);
+        list.add(new pair(item,tab));
+    }
+
+    @SubscribeEvent
+    public static void rList(BuildCreativeModeTabContentsEvent event) {
+        for (int i = 0; i < list.size(); i++) {
+            pair p = list.get(i);
+            if (event.getTabKey() == p.tab){
+                event.accept(p.item);
+            };
+        }
+    }
+
+    private record pair(RegistryObject<Item> item, ResourceKey<CreativeModeTab> tab){
+
     }
 }
